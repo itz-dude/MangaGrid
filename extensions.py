829eecf4f24e7f8469ed0f324a28dafc39b7c1e7
grigio888 <vinicius_grigio@hotmail.com>
2022-07-22 22:49:41 +0100

    Starting the translation process.
    - Only .py and .html are currently beign translated.
    - Need to figure out how to translate .js files.

--- extensions.py
+++ extensions.py
@@ -3,7 +3,8 @@
 # ------------------------------------------------- #
 import os
 
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel, gettext
 from flask_cors import CORS
 from flask_minify import Minify
 from flask_sqlalchemy import SQLAlchemy
@@ -13,22 +14,30 @@ from flask_sqlalchemy import SQLAlchemy
 # ------------------ BEHAVIORS -------------------- #
 # ------------------------------------------------- #
 
+# --------------------- APP ----------------------- #
 def return_flask_app():
     app = Flask(__name__)
 
+    app.config['BABEL_DEFAULT_LOCALE'] = 'pt'
     app.config['JSON_AS_ASCII'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///persistent/db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = os.urandom(24)
     Minify(app=app, html=True, js=True, cssless=True)
     CORS(app)
+    Babel(app)
+    
 
     return app
 
+# ---------------------- DB ----------------------- #
 db = SQLAlchemy(return_flask_app())
+babel = Babel(return_flask_app())
 
-
-
+# ----------------- TRANSLATIONS ------------------ #
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['pt', 'en'])
 
 # ------------------------------------------------- #
 # ------------------- SOURCES --------------------- #
