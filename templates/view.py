5284aab52d1791740cde9989f438a2775693d004
grigio888 <vinicius_grigio@hotmail.com>
2022-07-31 21:45:09 +0100

    Fixed a leftover code.

--- templates/view.py
+++ templates/view.py
@@ -89,7 +89,6 @@ def profile():
 @render.route('/profile/favorite')
 @render.route('/favorite')
 def favorite():
-    session['email'] = 'admin@admin.com'
     if 'email' not in session:
         return redirect('/login')
         
