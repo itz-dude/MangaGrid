d018fb8a8ab36e53cebe2fb762f3d7796c3b7f11
grigio888 <vinicius_grigio@hotmail.com>
2022-08-02 19:14:21 +0000

    Improved Mangaschan behavior.

--- manga/modules/mangaschan.py
+++ manga/modules/mangaschan.py
@@ -39,7 +39,10 @@ class Mangaschan(MangaScrapping):
         for item in div:
             manga_link = item.find('a')[0]
             title = manga_link.attrs['title']
-            image = manga_link.find('img')[0].attrs['src'] #.attrs['data-lazy-src']
+            try:
+                image = manga_link.find('img')[0].attrs['data-lazy-src']
+            except:
+                image = manga_link.find('img')[0].attrs['src'] #.attrs['data-lazy-src']
 
             try:
                 ext = item.find('div.bigor')[0]
@@ -80,7 +83,12 @@ class Mangaschan(MangaScrapping):
         for item in div:
             manga_link = item.find('a')[0]
             title = manga_link.attrs['title']
-            image = manga_link.find('img')[0].attrs['src']
+
+            try:
+                image = manga_link.find('img')[0].attrs['data-lazy-src']
+            except:
+                image = manga_link.find('img')[0].attrs['src']
+
             updated = 'unknown'
 
             search[title.replace('\n', '')] = {
@@ -110,7 +118,10 @@ class Mangaschan(MangaScrapping):
         panel = panel.find('div.postbody')[0]
         panel = panel.find('div.seriestucon')[0]
 
-        image = panel.find('img.wp-post-image')[0].attrs['src'] #.attrs['data-lazy-src']
+        try:
+            image = panel.find('img.wp-post-image')[0].attrs['data-lazy-src']
+        except:
+            image = panel.find('img.wp-post-image')[0].attrs['src'] #.attrs['data-lazy-src']
 
         title = panel.find('div.seriestuheader')[0].find('h1.entry-title')[0].text
 
